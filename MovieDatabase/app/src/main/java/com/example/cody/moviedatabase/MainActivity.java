4cac84793bce028f46748cad9f6c8079c59e05c1
Jonathan Brodie <brodiej@carleton.edu>
2015-04-09 13:33:33 -0500

    Trailer works now

--- MovieDatabase/app/src/main/java/com/example/cody/moviedatabase/MainActivity.java
+++ MovieDatabase/app/src/main/java/com/example/cody/moviedatabase/MainActivity.java
@@ -10,23 +10,30 @@ import android.widget.TextView;
 import android.widget.Toast;
 import java.util.ArrayList;
 import android.app.ListActivity;
+import android.widget.Spinner;
+
 
 
 public class MainActivity extends ListActivity {
-
+    private final String infoString="Information";
+    private final String trailerString="Movie Trailer";
+    private final String reviewString="Reviews";
+    private Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ArrayList<String> listItems = new ArrayList<String>();
-        listItems.add("Information");
-        listItems.add("Movie Trailer");
-        listItems.add("Reviews");
+        listItems.add(this.infoString);
+        listItems.add(this.trailerString);
+        listItems.add(this.reviewString);
 
         ListDemoAdapter adapter = new ListDemoAdapter(this, R.layout.list_cell, listItems);
         ListView listView = (ListView) this.findViewById(android.R.id.list);
         listView.setAdapter(adapter);
+        this.spinner=(Spinner)findViewById(R.id.spinner);
+        this.spinner.setOnItemSelectedListener(new SpinnerListener());
     }
 
     @Override
@@ -53,14 +60,28 @@ public class MainActivity extends ListActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
+    private String getCurrentMovie() {
+        return String.valueOf(spinner.getSelectedItem());
+    }
     @Override
     public void onListItemClick(ListView parent, View v, int position, long id) {
         TextView textView = (TextView)v.findViewById(R.id.listCellTextView);
-        if (v != null && textView != null) {
-            Toast toast = Toast.makeText(v.getContext(), textView.getText(), 0);
-            toast.show();
+
+        Intent newIntent;
+        System.out.println(":(");
+        if (textView.getText().equals(this.trailerString)) {
+            System.out.println("foo");
+            newIntent=new Intent(this, TrailerActivity.class);
+            System.out.println(this.getCurrentMovie());
+            newIntent.putExtra(TrailerActivity.RAW_NAME,this.getCurrentMovie());
         }
+        else {
+            Toast toast = Toast.makeText(v.getContext(), textView.getText(), Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
+        startActivity(newIntent);
+        overridePendingTransition(R.anim.slide_in_from_right, R.anim.slide_out_to_left);
     }
 
 }
